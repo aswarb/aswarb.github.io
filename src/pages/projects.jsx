5888ee7d85a730f20da1fbf3f06da3603510c5c2
Andrew Swarbrick <andrewoswarbrick@hotmail.co.uk>
2025-03-27 13:48:15 +0000

    basic project full page working

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -38,23 +38,66 @@ function ContextProvider({ children }) {
     )
 }
 
-export function Projects() {
-    const projects = projectMap.projects
+function ProjectPage(url) {
+    const context = useContext(projectImportUrl)
+    return (
+        <div
+            onClick={() => {
+                context.setValue(null)
+            }}
+        >
+            {' '}
+            {url}{' '}
+        </div>
+    )
+}
+
+function ProjectCards(projects) {
     let count = 0
+    return (
+        <div>
+            {projects.projects.map((project) => (
+                <ProjectCard
+                    key={(count += 1)}
+                    thumbnail={project.thumbnail}
+                    title={project.title}
+                    shortDescription={project.shortDescription}
+                    link={project.fullContentLink}
+                />
+            ))}
+        </div>
+    )
+}
+
+function ProjectFullPage(url) {
+    const context = useContext(projectImportUrl)
+
+    return <div onClick={() => context.setValue(null)}> {url.url} </div>
+}
+
+function PageContent() {
+    const context = useContext(projectImportUrl)
+
+    const projects = projectMap.projects
+    console.log(projects)
+    return (
+        <div>
+            {context.value === null ? (
+                <ProjectCards projects={projects} />
+            ) : (
+                <ProjectFullPage url={context.value} />
+            )}{' '}
+        </div>
+    )
+}
+
+export function Projects() {
     return (
         <div className="content">
             <ContextProvider>
-                <h3>projects</h3>
                 <div>
-                    {projects.map((project) => (
-                        <ProjectCard
-                            key={(count += 1)}
-                            thumbnail={project.thumbnail}
-                            title={project.title}
-                            shortDescription={project.shortDescription}
-                            link={project.fullContentLink}
-                        />
-                    ))}
+                    <h3>projects</h3>
+                    <PageContent />
                 </div>
             </ContextProvider>
         </div>
