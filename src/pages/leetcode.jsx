a4581c2e1b108ab505d1da292201f9ba130c1803
Andrew Swarbrick <andrewoswarbrick@hotmail.co.uk>
2025-04-17 00:28:15 +0100

    solutions isolated, need mapping

--- src/pages/leetcode.jsx
+++ src/pages/leetcode.jsx
@@ -8,20 +8,45 @@ function GetAllFiles(user, repoName) {
     return useFetch(queryString)
 }
 
-function getFileContent(path) {
-    return useFetch(
-        `https://raw.githubusercontent.com/aswarb/leetcodeSolutions/main/${path}`,
-        'text',
-    )
+function getFileContent(path, type = 'text') {
+    return useFetch(`https://raw.githubusercontent.com/aswarb/leetcodeSolutions/main/${path}`, type)
 }
 
-function Solution({ path }) {
-    const [contents, setContents] = useState()
-    const data = getFileContent(path, 'text')
+function Solution({ jsonDataPath }) {
+    const [jsonData, setJsonData] = useState('')
+    const [solutions, setSolutions] = useState([])
+    const data = getFileContent(jsonDataPath, 'json')
+    const currentPath = jsonDataPath.slice(0, jsonDataPath.lastIndexOf('/') + 1)
+
     useEffect(() => {
-        setContents(data)
+        setJsonData(data)
+        const paths = Object.keys(jsonData?.languages ?? {}).map((k) => {
+            const file = jsonData.languages[k].file
+            return `https://raw.githubusercontent.com/aswarb/leetcodeSolutions/main/${currentPath}${file.slice(2,file.length)}`
+        })
+
+        const solutionsArr = []
+        paths.forEach((p) => {
+            fetch(p)
+                .then((res) => res.text())
+                .then((d) => {
+                    console.log(p)
+
+                    solutionsArr.push(d)
+                })
+        })
+        setSolutions(solutionsArr)
     }, [data])
-    return <div style={{ whiteSpace: 'pre-wrap', overflow: 'visible' }}>{String(contents)}</div>
+
+    console.log(solutions)
+    
+    return (
+        <Collapsible title={jsonData?.name}>
+            {solutions?.map((s) => (
+                <div style={{whiteSpace:'pre-wrap'}}>{s}</div>
+            ))}
+        </Collapsible>
+    )
 }
 
 function SolutionList() {
@@ -30,15 +55,19 @@ function SolutionList() {
     const [blobs, setBlobs] = useState([])
 
     useEffect(() => {
-        setBlobs(allFiles.tree?.filter((el) => el.type === 'blob' && el.path.includes('json') && el.path.includes('src/')))
+        setBlobs(
+            allFiles.tree?.filter(
+                (el) => el.type === 'blob' && el.path.includes('json') && el.path.includes('src/'),
+            ),
+        )
     }, [allFiles])
 
     return (
         <div>
             {blobs?.map((b, index) => (
-                <Collapsible title={b.path} key={index}>
-                    <Solution path={b.path} />
-                </Collapsible>
+                <div key={index}>
+                    <Solution jsonDataPath={b.path} />
+                </div>
             ))}
         </div>
     )
